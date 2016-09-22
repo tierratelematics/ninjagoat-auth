449563e0d036d44f6f39d2218c097d523ab9a324
Claudio Iabichino <claudio.iabichino@synthesis3.it>
2016-09-22 11:11:34 +0200

    Fix Auth0Provider export

--- scripts/ninjagoat-auth.ts
+++ scripts/ninjagoat-auth.ts
@@ -1,4 +1,5 @@
 /// <reference path="../typings/index.d.ts" />
 export {default as AuthModule} from "./AuthModule";
 export {default as AuthHttpClient} from "./auth0/AuthHttpClient";
+export {default as Auth0Provider} from "./auth0/Auth0Provider";
 export {default as Authorized} from "./AuthorizedDecorator";
