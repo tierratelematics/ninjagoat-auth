ab5b7f9e4ba6ca167348a03275d5c10f7a02a9f0
Michele Melluso <mmelluso@topcon.com>
2018-02-23 15:38:12 +0100

    Auto genarate declarations

---
+++ scripts/index.ts
@@ -0,0 +1,14 @@
+export { default as IAuthProvider } from "./interfaces/IAuthProvider";
+export { default as IAuthDataRetriever } from "./interfaces/IAuthDataRetriever";
+export { default as IAuthErrorHandler } from "./interfaces/IAuthErrorHandler";
+export { default as ISessionChecker } from "./interfaces/ISessionChecker";
+export { default as ILocationNavigator } from "./interfaces/ILocationNavigator";
+export { default as IAuthConfig } from "./interfaces/IAuthConfig";
+export { default as AuthModule } from "./AuthModule";
+export { default as AuthStage } from "./AuthStage";
+export { default as AuthHttpClient } from "./auth0/AuthHttpClient";
+export { default as Auth0Provider } from "./auth0/Auth0Provider";
+export { default as Authorized } from "./AuthorizedDecorator";
+export { default as AuthRouteStrategy } from "./auth0/AuthRouteStrategy";
+export { default as SessionChecker } from "./auth0/SessionChecker";
+export { default as AuthErrorHandler } from "./auth0/AuthErrorHandler";
